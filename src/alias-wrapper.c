a135a5a08f7a54f5018204c33b9594aa01f75e3f
bwarsaw <>
1998-05-01 04:23:36 +0000

    Some inconsequential formatting changes.
    - Use "Python" C editing style when used with X/Emacs
    
    - Make X/Emacs friendly by putting function opening braces in column
      zero and putting function name on separate line from return type
    
    - Also added a C local variables section

--- src/alias-wrapper.c
+++ src/alias-wrapper.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 
-const int  LEGAL_PARENT_UID = 9001;  /* mailman's UID */
-const int  LEGAL_PARENT_GID = 6; /* mailman's GID */
+const int  LEGAL_PARENT_UID = 9001;	     /* mailman's UID */
+const int  LEGAL_PARENT_GID = 6;	     /* mailman's GID */
 
 const char* SENDMAIL_CMD = "/usr/sbin/sendmail";
 const char* ALIAS_FILE   = "/etc/aliases";
@@ -11,75 +11,82 @@ const char* WRAPPER      = "/home/mailman/mailman/mail/wrapper";
 /* 
 ** is the parent process allowed to call us?
 */
-int LegalCaller() 
+int
+LegalCaller() 
 {
-    /* compare to our parent's uid */
-    if(LEGAL_PARENT_UID != getuid()) 
-      {
-	printf("GOT UID %d.\n", getuid());
-        return 0;
-      }
-    if(LEGAL_PARENT_GID != getgid())
-      {
-	printf("GOT GID %d.\n", getgid());
-        return 0;
-      }
-    return 1;
+	/* compare to our parent's uid */
+	if (LEGAL_PARENT_UID != getuid()) {
+		printf("GOT UID %d.\n", getuid());
+		return 0;
+	}
+	if (LEGAL_PARENT_GID != getgid()) {
+		printf("GOT GID %d.\n", getgid());
+		return 0;
+	}
+	return 1;
 }
 
-void AddListAliases(char *list)
+
+void
+AddListAliases(char *list)
 {
-  FILE *f;
-  int  err = 0;
+	FILE *f;
+	int  err = 0;
 
-  f = fopen(ALIAS_FILE ,"a+");
-  if (f == NULL)
-    {
-      err = 1;
-      f = stderr;
-      fprintf(f, "\n\n***********ERROR!!!!***********\n");
-      fprintf(f, "Could not write to the /etc/aliases file.\n");
-      fprintf(f, "Please become root, add the lines below to that file,\n");
-      fprintf(f, "And then run the command %s -bi\n", SENDMAIL_CMD);
-    }
+	f = fopen(ALIAS_FILE ,"a+");
+	if (f == NULL) {
+		err = 1;
+		f = stderr;
+		fprintf(f, "\n\n***********ERROR!!!!***********\n");
+		fprintf(f, "Could not write to the /etc/aliases file.\n");
+		fprintf(f, "Please become root, add the lines below to\n");
+		fprintf(f, "that file, and then run the command:\n");
+		fprintf(f, "%s -bi\n", SENDMAIL_CMD);
+	}
 
-  fprintf(f, "\n\n#-- %s -- mailing list aliases:\n", list);
-  fprintf(f, "%s: \t|\"%s post %s\"\n", list, WRAPPER, list);
-  fprintf(f, "%s-admin: \t|\"%s mailowner %s\"\n", list, WRAPPER, list);
-  fprintf(f, "%s-request: \t|\"%s mailcmd %s\"\n", list, WRAPPER, list);
-  fprintf(f, "# I think we don't want this one... it'll change the unix from line...\n");
-  fprintf(f, "#owner-%s: \t%s-admin\n", list, list);
-  fprintf(f, "#%s-owner: \t%s-admin\n", list, list);
-  fprintf(f, "\n");
-  fclose(f);
+	fprintf(f, "\n\n#-- %s -- mailing list aliases:\n", list);
+	fprintf(f, "%s: \t|\"%s post %s\"\n", list, WRAPPER, list);
+	fprintf(f, "%s-admin: \t|\"%s mailowner %s\"\n", list, WRAPPER, list);
+	fprintf(f, "%s-request: \t|\"%s mailcmd %s\"\n", list, WRAPPER, list);
+	fprintf(f, "# I think we don't want this one...\n");
+	fprintf(f, "it'll change the unix from line...\n");
+	fprintf(f, "#owner-%s: \t%s-admin\n", list, list);
+	fprintf(f, "#%s-owner: \t%s-admin\n", list, list);
+	fprintf(f, "\n");
+	fclose(f);
 
-  if (!err)
-    {
-      printf("Rebuilding alias database...\n");
-      execlp(SENDMAIL_CMD, SENDMAIL_CMD, "-bi");
-    }
+	if (!err) {
+		printf("Rebuilding alias database...\n");
+		execlp(SENDMAIL_CMD, SENDMAIL_CMD, "-bi");
+	}
 }
 
-int main(int argc, char **argv, char **env) 
-{
-  char  *command;
-  int   i;
 
-  if(argc != 2)
-    {
-      printf("Usage: %s [list-name]\n", argv[0]);
-      exit(0);
-    }
-  if(LegalCaller()) 
-    {
-      setuid(geteuid());
-      AddListAliases(argv[1]);
-    }
-  else
-    {
-      printf("Illegal caller!\n");
-      return 1;
-    }
-  return 0;
+int
+main(int argc, char **argv, char **env) 
+{
+	char  *command;
+	int   i;
+
+	if (argc != 2) {
+		printf("Usage: %s [list-name]\n", argv[0]);
+		exit(0);
+	}
+	if (LegalCaller()) {
+		setuid(geteuid());
+		AddListAliases(argv[1]);
+	}
+	else {
+		printf("Illegal caller!\n");
+		return 1;
+	}
+	return 0;
 }
 
+
+
+/*
+ * Local Variables:
+ * c-file-style: "python"
+ * End:
+ */
--- src/mail-wrapper.c
+++ src/mail-wrapper.c
@@ -3,12 +3,13 @@
 ** variable, and pass it to two commands.
 **
 ** 10-17-96 : Hal Schechner
-** 12-14-96 : John Viega -- changed to work on 1 command, 
-**                          take a list of valid commands,
-**                          just pass on argv, and use execvp()
-**                          Also threw in some useful feedback for when there's
-**                          a failure, mainly for future debugging.
-**                          Made it a root script so we could call setuid()
+**
+** 12-14-96 : John Viega -- changed to work on 1 command, take a list of
+**                          valid commands, just pass on argv, and use
+**                          execvp() Also threw in some useful feedback for
+**                          when there's a failure, mainly for future
+**                          debugging.  Made it a root script so we could
+**                          call setuid()
 **
 ** Chmod this bitch 4755.
 **
@@ -21,116 +22,130 @@ extern int errno;
 FILE *f;
 
 const char *VALID_COMMANDS[] = {
-  "post", 
-  "mailcmd",
-  "mailowner",
-  NULL /* Sentinal, don't remove */
+	"post", 
+	"mailcmd",
+	"mailowner",
+	NULL				     /* Sentinal, don't remove */
 };
 
 
-/* Might want to make this full path.  
-   I can write whatever program named sendmail,
-   so this isn't much for security.
+/* Might want to make this full path.  I can write whatever program named
+ * sendmail, so this isn't much for security.
 */
 const char *LEGAL_PARENT_NAMES[] = {
-  "sendmail",
-  NULL /* Sentinal, don't remove */
+	"sendmail",
+	NULL				     /* Sentinal, don't remove */
 };
 
+
 /* Should make these arrays too... */
-const int  LEGAL_PARENT_UID = 1;  /* mail's UID */
-const int  LEGAL_PARENT_GID = 1; /* mail's GID */
+const int  LEGAL_PARENT_UID = 1;	     /* mail's UID */
+const int  LEGAL_PARENT_GID = 1;	     /* mail's GID */
 
 
 /*
 ** what is the name of the process with pid of 'pid'
 */
-char *get_process_name(int pid) {
-    FILE *proc;
-    char fname[30];
-    char tmp[255];
-    static char procname[255];
-    sprintf(fname, "/proc/%d/status", pid);
-    proc = fopen(fname, "r");
-    fgets(tmp, 256, proc);
-    sscanf(tmp, "Name:   %s\n", procname);
-    fclose(proc);
-    return procname;
+char *
+get_process_name(int pid)
+{
+	FILE *proc;
+	char fname[30];
+	char tmp[255];
+	static char procname[255];
+
+	sprintf(fname, "/proc/%d/status", pid);
+	proc = fopen(fname, "r");
+	fgets(tmp, 256, proc);
+	sscanf(tmp, "Name:   %s\n", procname);
+	fclose(proc);
+	return procname;
 }
 
 
-int valid_parent(char *parent){
-  int i = 0;
+int
+valid_parent(char *parent)
+{
+	int i = 0;
 
-  while(LEGAL_PARENT_NAMES[i] != NULL)
-    {
-      if(!strcmp(parent, LEGAL_PARENT_NAMES[i]))
-	{
-	  return 1;
+	while (LEGAL_PARENT_NAMES[i] != NULL) {
+		if (!strcmp(parent, LEGAL_PARENT_NAMES[i])) {
+			return 1;
+		}
+		i++;
 	}
-      i++;
-    }
-  return 0;
+	return 0;
 }
+
 
 /* 
 ** is the parent process allowed to call us?
 */
-int legal_caller() {
-    /* compare to our parent's uid */
-    if(LEGAL_PARENT_UID != getuid()) 
-      {
-	/*	fprintf(f,"GOT UID %d.\n", getuid()); */
-	printf("GOT UID %d.\n", getuid());
-        return 0;
-      }
-    if(LEGAL_PARENT_GID != getgid())
-      {
-	/* fprintf(f,"GOT GID %d.\n", getgid()); */
-	printf("GOT GID %d.\n", getgid());
-        return 0;
-      }
-    return 1;
-}
-
-int valid_command(char *command){
-  int i = 0;
-
-  while(VALID_COMMANDS[i] != NULL)
-    {
-      if(!strcmp(command, VALID_COMMANDS[i]))
-	{
-	  return 1;
+int
+legal_caller()
+{
+	/* compare to our parent's uid */
+	if (LEGAL_PARENT_UID != getuid()) {
+		/*	fprintf(f,"GOT UID %d.\n", getuid()); */
+		printf("GOT UID %d.\n", getuid());
+		return 0;
 	}
-      i++;
-    }
-  return 0;
+	if (LEGAL_PARENT_GID != getgid()) {
+		/* fprintf(f,"GOT GID %d.\n", getgid()); */
+		printf("GOT GID %d.\n", getgid());
+		return 0;
+	}
+	return 1;
 }
 
-int main(int argc, char **argv) {
-  char  *command;
-  int   i;
+
+int
+valid_command(char *command)
+{
+	int i = 0;
+
+	while (VALID_COMMANDS[i] != NULL) {
+		if (!strcmp(command, VALID_COMMANDS[i])) {
+			return 1;
+		}
+		i++;
+	}
+	return 0;
+}
+
+
+int
+main(int argc, char **argv)
+{
+	char  *command;
+	int   i;
   
-  if(argc < 2)
-    {
-      printf("Usage: %s program [args...]\n", argv[0]);
-      fflush(stdout);
-      exit(0);
-    }
-  i = strlen(argv[1]) + strlen(COMMAND_LOCATION) + 2;
-  command = (char *)malloc(sizeof(char) * i);
-  sprintf(command, "%s/%s", COMMAND_LOCATION, argv[1]);
+	if (argc < 2) {
+		printf("Usage: %s program [args...]\n", argv[0]);
+		fflush(stdout);
+		exit(0);
+	}
+	i = strlen(argv[1]) + strlen(COMMAND_LOCATION) + 2;
+	command = (char *)malloc(sizeof(char) * i);
+	sprintf(command, "%s/%s", COMMAND_LOCATION, argv[1]);
 
-  if(!valid_command(argv[1])){
-    printf("Illegal command.\n");
-    }
-  else{
-    if(legal_caller()) {
-      setuid(geteuid());
-      execv(command, &argv[1]);
-    }
-    else {
-      printf("Illegal caller!\n");
-    }
-  }
+	if (!valid_command(argv[1])) {
+		printf("Illegal command.\n");
+	}
+	else {
+		if (legal_caller()) {
+			setuid(geteuid());
+			execv(command, &argv[1]);
+		}
+		else {
+			printf("Illegal caller!\n");
+		}
+	}
 }
+
+
+/*
+ * Local Variables:
+ * c-file-style: "python"
+ * End:
+ */
