2b3256843447fa2d17cb47fa1aa67462d9f45dd8
viega <>
1998-06-14 02:13:02 +0000

    There's now a global USE_CRYPT variable.  It *should* be set by the autoconf stuff.  Basically, the autoconf script should fire up Python and see if crypt can be imported.  Of course, if a previous installation has a different value than configure determined, it should be used instead, otherwise stuff will mess up.

--- Mailman/Crypt.py
+++ Mailman/Crypt.py
@@ -1,6 +1,8 @@
-try:
+import mm_cfg
+
+if mm_cfg.USE_CRYPT:
     from crypt import *
-except ImportError:
+else:
     def crypt(string, seed):
         import md5
         return md5.new(string).digest()
