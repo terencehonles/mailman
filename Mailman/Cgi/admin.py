58d31e44e370f7598b3c2fa0377e4eac572028a0
bwarsaw <>
1999-03-09 02:41:44 +0000

    split a long line

--- Mailman/Cgi/admin.py
+++ Mailman/Cgi/admin.py
@@ -771,7 +771,8 @@ def ChangeOptions(lst, category, cgi_info, document):
 	names = string.split(name_text, '\n')
         subscribe_success = []
         subscribe_errors = []
-        send_welcome_msg = string.atoi(cgi_info["send_welcome_msg_to_this_batch"].value)
+        send_welcome_msg = string.atoi(
+            cgi_info["send_welcome_msg_to_this_batch"].value)
 	for new_name in map(string.strip, names):
             new_name = Utils.ParseAddrs(new_name)
             digest = 0
