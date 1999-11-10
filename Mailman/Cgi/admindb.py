0ac6810179ee6e58b25d3b4444695c13346c8f7b
bwarsaw <>
1999-11-10 20:27:32 +0000

    Massively restructured for integration with the new request API (i.e. ListAdmin.py).  Also several changes to the layout of request pages that I think make using them much nicer.
    - There's a preamble which actually explains what you're supposed to
      do on this page.
    
    - There's only two "Submit All Data" buttons.  I found the multiple
      submit buttons just too darn confusing (hard to explain == hard to
      use).  Guido concurred.
    
    - The tables have no cellpadding so they aren't as spread out.
    
    - Each posting hold tells you "N of #" so you know how many more are
      shown below.
    
    - Layout of posting holds has changed so you see immediately 1) who
      it's from; 2) why the message was held; 3) what the subject of the
      message is.  Next you see the action buttons, followed by the
      explanation text box.  Finally you see the full text of the message
      in a separate text box.  This should make handling extremely long
      messages much easier because they don't clutter up the main page too
      much.
    
      Also, Guido had an excellent suggestion: Let the admin edit the full
      text of the message in this box!  Probably wouldn't be to hard to
      do, actually.

--- Mailman/Cgi/admindb.py
+++ Mailman/Cgi/admindb.py
@@ -25,63 +25,56 @@ from Mailman.htmlformat import *
 from Mailman import mm_cfg
 
 
+
+def handle_no_list(doc, extra=''):
+    doc.SetTitle('Mailman Admindb Error')
+    doc.AddItem(Header(2, 'Mailman Admindb Error'))
+    doc.AddItem(extra)
+    doc.AddItem('You must specify a list name.  Here is the ')
+    link = mm_cfg.DEFAULT_URL
+    if link[-1] <> '/':
+        link = link + '/'
+    link = link + 'admin'
+    doc.AddItem(Link(link, 'list of available mailing lists.'))
+    print doc.Format(bgcolor="#ffffff")
+    sys.exit(0)
+
+
+
 def main():
-    # XXX: Yuk, blech, ick
-    global list
-    global form
-    global doc
-    global list_name
-
     doc = Document()
-
+    # figure out which list we're going to process
     try:
         path = os.environ['PATH_INFO']
     except KeyError:
-        doc.SetTitle("Admindb Error")
-        doc.AddItem(
-            Header(2, "You must specify a list."))
-        print doc.Format(bgcolor="#ffffff")
-        sys.exit(0)
-
-    list_info = Utils.GetPathPieces(path)
-
-    if len(list_info) < 1:
-        doc.SetTitle("Admindb Error")
-        doc.AddItem(Header(2, "You must specify a list."))
-        print doc.Format(bgcolor="#ffffff")
-        sys.exit(0)
-    list_name = string.lower(list_info[0])
-
+        handle_no_list(doc)
+    # get URL components.  the list name should be the zeroth part
+    parts = Utils.GetPathPieces(path)
     try:
-        list = MailList.MailList(list_name)
-    except:
-        msg = "%s: No such list." % list_name
-        doc.SetTitle("Admindb Error - %s" % msg)
-        doc.AddItem(Header(2, msg))
-        print doc.Format(bgcolor="#ffffff")
-        sys.exit(0)
-
-    if not list._ready:
-        msg = "%s: No such list." % list_name
-        doc.SetTitle("Admindb Error - %s" % msg)
-        doc.AddItem(Header(2, msg))
-        print doc.Format(bgcolor="#ffffff")
-        sys.exit(0)
-
+        listname = string.lower(parts[0])
+    except IndexError:
+        handle_no_list(doc)
+    # now that we have the list name, create the list object
+    try:
+        mlist = MailList.MailList(listname)
+    except (Errors.MMUnknownListError, Errors.MMListNotReady):
+        handle_no_list(doc, 'No such list: <tt>%s</tt><p>' % listname)
+    #
+    # now we must authorize the user to view this page, and if they are, to
+    # handle both the printing of the current outstanding requests, and the
+    # selected actions
     try:
         form = cgi.FieldStorage()
-
         # Authenticate.
         is_auth = 0
         adminpw = None
-        message = ""
-
+        message = ''
+        # has the user already authenticated?
         if form.has_key('adminpw'):
             adminpw = form['adminpw'].value
         try:
             # admindb uses the same cookie as admin
-            is_auth = list.WebAuthenticate(password=adminpw,
-                                           cookie='admin')
+            is_auth = mlist.WebAuthenticate(password=adminpw, cookie='admin')
         except Errors.MMBadPasswordError:
             message = 'Sorry, wrong password.  Try again.'
         except Errors.MMExpiredCookieError:
@@ -91,94 +84,139 @@ def main():
             message = 'Error decoding authorization cookie.'
         except Errors.MMAuthenticationError:
             message = 'Authentication error.'
-            
+        #
+        # Not authorized, so give them a chance to head over to the login page
         if not is_auth:
-            defaulturi = '/mailman/admindb%s/%s' % (mm_cfg.CGIEXT, list_name)
+            uri = '/mailman/admindb%s/%s' % (mm_cfg.CGIEXT, listname)
             if message:
                 message = FontAttr(
                     message, color='FF5060', size='+1').Format()
             print 'Content-type: text/html\n\n'
             text = Utils.maketext(
                 'admlogin.txt',
-                {'listname': list_name,
-                 'path'    : Utils.GetRequestURI(defaulturi),
+                {'listname': listname,
+                 'path'    : Utils.GetRequestURI(uri),
                  'message' : message,
                  })
             print text
             return
-
+        #
+        # If this is a form submission, then we'll process the requests and
+        # print the results.  otherwise (there are no keys in the form), we'll
+        # print out the list of pending requests
+        #
         if len(form.keys()):
-            doc.SetTitle("%s Admindb Results" % list.real_name)
-            HandleRequests(doc)
+            doc.SetTitle("%s Admindb Results" % mlist.real_name)
+            HandleRequests(mlist, doc, form)
         else:
-            doc.SetTitle("%s Admindb" % list.real_name)
-        PrintRequests(doc)
+            doc.SetTitle("%s Admindb" % mlist.real_name)
+        PrintRequests(mlist, doc, form)
         text = doc.Format(bgcolor="#ffffff")
         print text
         sys.stdout.flush()
     finally:
-        list.Unlock()
+        mlist.Unlock()
 
 
 
-# Note, these 2 functions use i only to count the number of times to
-# go around.  We always operate on the first element of the list
-# because we're going to delete the element after we operate on it.
+def PrintRequests(mlist, doc, form):
+    # The only types of requests we know about are add member and post.
+    # Anything else that might have gotten in here somehow we'll just ignore
+    # (This should never happen unless someone is hacking at the code).
+    doc.AddItem(Header(2, 'Administrative requests for mailing list: <em>' +
+                       mlist.real_name + '</em>'))
+    # short circuit for when there are no pending requests
+    if not mlist.NumRequestsPending():
+	doc.AddItem('There are no pending requests.  You can now ')
+        doc.AddItem(
+            Link(mlist.GetRelativeScriptURL('admin'),
+                 Italic('view or edit the list configuration information.')))
+	doc.AddItem(mlist.GetMailmanFooter())
+	return
 
-def SubscribeAll():
-    # XXX: Yuk, blech, ick
-    global list
-    global form
-    for i in range(len(list.requests['add_member'])):
-	comment_key = 'comment-%d' % list.requests['add_member'][0][0]
-	if form.has_key(comment_key):
-	    list.HandleRequest(('add_member', 0), 1, form[comment_key].value)
-	else:
-	    list.HandleRequest(('add_member', 0), 1)
+    doc.AddItem(Utils.maketext(
+        'admindbpreamble.html', {'listname': mlist.real_name}, raw=1))
+    doc.AddItem(
+        Link(mlist.GetRelativeScriptURL('admin'),
+             Italic('view or edit the list configuration information')))
+    doc.AddItem('.<p>')
+    form = Form(mlist.GetRelativeScriptURL('admindb'))
+    doc.AddItem(form)
+    form.AddItem(SubmitButton('submit', 'Submit All Data'))
+    #
+    # Add the subscription request section
+    subpendings = mlist.GetSubscriptionIds()
+    if subpendings:
+        form.AddItem('<hr>')
+	form.AddItem(Center(Header(2, 'Subscription Requests')))
+	t = Table(border=2)
+	t.AddRow([
+	    Bold('Address'),
+	    Bold('Your Decision'),
+	    Bold('Reason for subscription refusal (optional)')
+            ])
+        for id in subpendings:
+	    PrintAddMemberRequest(mlist, id, t)
+	form.AddItem(t)
+    # Post holds are now handled differently
+    heldmsgs = mlist.GetHeldMessageIds()
+    total = len(heldmsgs)
+    if total:
+        count = 1
+        for id in heldmsgs:
+            info = mlist.GetRecord(id)
+            PrintPostRequest(mlist, id, info, total, count, form)
+            count = count + 1
+    form.AddItem('<hr>')
+    form.AddItem(SubmitButton('submit', 'Submit All Data'))
+    doc.AddItem(mlist.GetMailmanFooter())
 
-def SubscribeNone():
-    # XXX: Yuk, blech, ick
-    global list
-    global form
-    for i in range(len(list.requests['add_member'])):
-	comment_key = 'comment-%d' % list.requests['add_member'][0][0]
-	if form.has_key(comment_key):
-	    list.HandleRequest(('add_member', 0), 0, form[comment_key].value)
-	else:
-	    list.HandleRequest(('add_member', 0), 0)
 
-def PrintHeader(str, error=0):
-    # XXX: blech, yuk, ick
-    global doc
+
+def PrintAddMemberRequest(mlist, id, table):
+    time, addr, passwd, digest = mlist.GetRecord(id)
+    table.AddRow([addr,
+                  RadioButtonArray(id, ('Refuse', 'Subscribe')),
+                  TextBox('comment-%d' % id, size=30)
+                  ])
 
-    if error:
-	it = FontAttr(str, color="ff5060")
-    else:
-	it = str
-    doc.AddItem(Header(3, Italic(it)))
-    doc.AddItem('<hr>')
+def PrintPostRequest(mlist, id, info, total, count, form):
+    ptime, sender, subject, reason, text = info
+    form.AddItem('<hr>')
+    msg = 'Posting Held for Approval'
+    if total <> 1:
+        msg = msg + ' (%d of %d)' % (count, total)
+    form.AddItem(Center(Header(2, msg)))
+    t = Table(cellspacing=0, cellpadding=0)
+    t.AddRow([Bold('From:'), sender])
+    t.AddRow([Bold('Reason:'), reason])
+    t.AddRow([Bold('Subject:'), subject])
+    t.AddRow([
+        Bold('Action:'),
+        RadioButtonArray(id, ("Approve", "Reject", "Discard (eg, spam)"))
+        ])
+    t.AddRow([
+	Bold('If you reject this post, explain (optional):'),
+	TextArea('comment-%d' % id, rows=4, cols=60,
+                 text=("Please do *not* post administrative requests"
+                       " to the mailing list!  If you wish to subscribe,"
+                       " visit %s or send a 'help' message to the"
+                       " the request address, %s , for instructions"
+                       % (mlist.GetAbsoluteScriptURL('listinfo'),
+                          mlist.GetRequestEmail())))
+        ])
+    row, col = t.GetCurrentRowIndex(), t.GetCurrentCellIndex()
+    t.AddCellInfo(row, col, colspan=3)
+    t.AddRow([Bold('Full Text:'),
+              TextArea('fulltext-%d' % id, text, rows=10, cols=60)])
+    row, col = t.GetCurrentRowIndex(), t.GetCurrentCellIndex()
+    t.AddCellInfo(row, col, colspan=3)
+    form.AddItem(t)
+    form.AddItem('<p>')
 
 
-def HandleRequests(doc):
-    # XXX: Yuk, blech, ick
-    global list
-    global form
-##     if not form.has_key('adminpw'):
-##	PrintHeader('You need to supply the admin password '
-##		    'to answer requests.', error=1)
-##	return
-##    try:
-##	list.ConfirmAdminPassword(form['adminpw'].value)
-##    except:
-##	PrintHeader('Incorrect admin password.', error=1)
-##	return
-    ignore_subscribes = 0
-    if form.has_key('subscribe_all'):
-	ignore_subscribes = 1
-	SubscribeAll()
-    elif form.has_key('subscribe_none'):
-	ignore_subscribes = 1
-	SubscribeNone()
+
+def HandleRequests(mlist, doc, form):
     for k in form.keys():
         formv = form[k]
         if type(formv) == types.ListType:
@@ -188,129 +226,19 @@ def HandleRequests(doc):
             request_id = int(k)
         except ValueError:
             continue
-	try:
-	    request = list.GetRequest(request_id)
-	except Errors.MMBadRequestId:
-	    continue # You've already changed the database.  No biggie.
-	if ignore_subscribes and request[0] == 'add_member':
-	    # We already handled this request.
-	    continue
-	comment_key = 'comment-%d' % request_id
-	if form.has_key(comment_key):
-	    list.HandleRequest(request, v, form[comment_key].value)
-	else:
-	    list.HandleRequest(request, v)
-    list.Save()
-    PrintHeader('Database Updated...')
-
-
-def PrintAddMemberRequest(val, table):
-    table.AddRow([
-	val[3], 
-	RadioButtonArray(val[0], ("Refuse", "Subscribe")),
-	TextBox("comment-%d" % val[0], size=50)
-	])
-
-def PrintPostRequest(val, form):
-    t = Table(cellspacing=10)
-    t.AddRow([
-        FontSize("+1", Bold('Post held because: ')),
-        val[3]
-        ])
-    t.AddRow([
-	FontSize("+1", Bold('Action to take on this post:')),
-	RadioButtonArray(val[0], ("Approve", "Reject", "Discard (eg, spam)")),
-	SubmitButton('submit', 'Submit All Data')
-        ])
-    t.AddRow([
-	FontSize("+1", Bold('If you reject this post, explain (optional):')),
-	TextArea("comment-%d" % val[0], rows=4, cols=60,
-                 text=("Please do *not* post administrative requests"
-                       " to the mailing list!  If you wish to subscribe,"
-                       " visit %s or send a 'help' message to the"
-                       " the request address, %s , for instructions"
-                       % (list.GetAbsoluteScriptURL('listinfo'),
-                          list.GetRequestEmail())))
-        ])
-
-    cur_row = t.GetCurrentRowIndex()
-    cur_col = t.GetCurrentCellIndex()
-    t.AddCellInfo(cur_row, cur_col, colspan=3)
-
-    t.AddRow([
-	FontSize("+1", Bold('Contents:'))
-        ])
-    form.AddItem(t)
-    form.AddItem(Preformatted(val[2][1]))
-    form.AddItem('<p>')
-
-
-
-def PrintRequests(doc):
-    # XXX: Yuk, blech, ick
-    global list
-    global form
-
-    # The only types of requests we know about are add_member and post.
-    # Anything else that might have gotten in here somehow we'll just
-    # ignore (This should never happen unless someone is hacking at
-    # the code).
-
-    doc.AddItem(Header(2, "Administrative requests for '%s' mailing list"
-                       % list.real_name))
-    doc.AddItem(FontSize("+1",
-                         Link(list.GetRelativeScriptURL('admin'),
-                              Italic(
-        'View or edit the list configuration information'))))
-    doc.AddItem('<p>')
-    if not list.NumRequestsPending():
-	doc.AddItem(Header(3,'There are no pending requests.'))
-	doc.AddItem(list.GetMailmanFooter())
-	return
-    form = Form(list.GetRelativeScriptURL('admindb'))
-    doc.AddItem(form)
-##    form.AddItem('Admin password: ')
-##    form.AddItem(PasswordBox('adminpw'))
-##    form.AddItem('<p>')
-    if list.requests.has_key('add_member'):
-##	form.AddItem('<hr>')
-## 	t = Table(cellspacing=10)
-## 	t.AddRow([
-## 	    SubmitButton('submit', 'Submit All Data'),
-## 	    SubmitButton('subscribe_all', 'Subscribe Everybody'),
-## 	    SubmitButton('subscribe_none', 'Refuse Everybody')
-## 	    ])
-## 	form.AddItem(t)
-##	form.AddItem('<hr>')
-	form.AddItem(Center(
-	    Header(2, 'Subscription Requests')))
-	t = Table(border=2)
-	t.AddRow([
-	    Bold('Email'),
-	    Bold('Decision'),
-	    Bold('Reasoning for subscription refusal (optional)')
-            ])
-	for request in list.requests['add_member']:
-	    PrintAddMemberRequest(request, t)
-
-	form.AddItem(t)
-	t = Table(cellspacing=10)
-	t.AddRow([
-	    SubmitButton('submit', 'Submit All Data'),
-	    SubmitButton('subscribe_all', 'Subscribe Everybody'),
-	    SubmitButton('subscribe_none', 'Refuse Everybody')
-	    ])
-	form.AddItem(t)
-
-	# Print submitit buttons...
-    if list.requests.has_key('post'):
-	for request in list.requests['post']:
-	    form.AddItem('<hr>')
-	    form.AddItem(Center(Header(2, "Held Message")))
-	    PrintPostRequest(request, form)
-    doc.AddItem(list.GetMailmanFooter())
-
-
-# copied from admin.py
-def AddErrorMessage(doc, errmsg, *args):
-    doc.AddItem(Header(3, Italic(FontAttr(errmsg % args, color="#ff66cc"))))
+        # get the action comment/reason if present
+        commentkey = 'comment-%d' % request_id
+        comment = '[No reason given]'
+        if form.has_key(commentkey):
+            comment = form[commentkey].value
+        #
+        # handle the request id
+        try:
+            mlist.HandleRequest(request_id, v, comment)
+        except KeyError:
+            # that's okay, it just means someone else has already updated the
+            # database, so just ignore this id
+            continue
+    # save the list and print the results
+    mlist.Save()
+    doc.AddItem(Header(2, 'Database Updated...'))
