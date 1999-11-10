b55034ff8b7ab71cda6350f4b7e4b3d161252a5c
bwarsaw <>
1999-11-10 17:50:38 +0000

    New pipeline delivery module
    """Decorate a message by sticking the header and footer around it.
    """

---
+++ Mailman/Handlers/Decorate.py
@@ -0,0 +1,31 @@
+# Copyright (C) 1998 by the Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software 
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""Decorate a message by sticking the header and footer around it.
+"""
+
+from Mailman import mm_cfg
+from Mailman import Utils
+
+
+
+def process(mlist, msg):
+    d = Utils.SafeDict(mlist.__dict__)
+    d['cgiext'] = mm_cfg.CGIEXT
+    # interpolate into the header
+    header = mlist.msg_header % d
+    footer = mlist.msg_footer % d
+    msg.body = header + msg.body + footer
