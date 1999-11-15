7de69d8754229dadeb3d4c9a126a8550b0501308
bwarsaw <>
1999-11-15 22:23:50 +0000

    MailmanError: new exception class which will eventually be the base class for all Mailman exception.  When creating a new exception class, or converting an old string-based exception, always use this as the base class.
    MMLoopingPost, EmailAddressError: Use MailmanError as base class.
    
    LostHeldMessage: New exception which handles the case when the held
    message text file gets lost (i.e. deleted from the file system) during
    an admindb transaction.

--- Mailman/Errors.py
+++ Mailman/Errors.py
@@ -24,15 +24,6 @@ MMBadListError       = "MMBadListError"
 MMBadUserError       = "MMBadUserError"
 MMBadConfigError     = "MMBadConfigError"
 
-# Exception hierarchy for bad email address errors that can be raised from
-# Utils.ValidateEmail()
-class EmailAddressError(Exception):
-    pass
-class MMBadEmailError(EmailAddressError):
-    pass
-class MMHostileAddress(EmailAddressError):
-    pass
-
 # Exception hierarchy for various authentication failures, can be
 # raised from functions in SecurityManager.py
 class MMAuthenticationError(Exception): pass
@@ -53,10 +44,6 @@ MMBadConfirmation    = "MMBadConfirmation"
 MMAlreadyAMember     = "MMAlreadyAMember"
 MMAlreadyDigested    = "MMAlreadyDigested"
 MMAlreadyUndigested  = "MMAlreadyUndigested"
-
-class MMLoopingPost:
-    """Post already went through this list!"""
-    pass
 
 MODERATED_LIST_MSG    = "Moderated list"
 IMPLICIT_DEST_MSG     = "Implicit destination"
@@ -85,3 +72,38 @@ Some useful fields are:
   <dd>The less brief list description.
 </dl>
 """
+
+
+
+# New style class based exceptions.  All the above errors should eventually be
+# converted.
+
+class MailmanError(Exception):
+    """Base class for all Mailman exceptions."""
+    pass
+
+
+class MMLoopingPost(MailmanError):
+    """Post already went through this list!"""
+    pass
+
+
+# Exception hierarchy for bad email address errors that can be raised from
+# Utils.ValidateEmail()
+class EmailAddressError(MailmanError):
+    """Base class for email address validation errors."""
+    pass
+
+class MMBadEmailError(EmailAddressError):
+    """Email address is invalid (empty string or not fully qualified)."""
+    pass
+
+class MMHostileAddress(EmailAddressError):
+    """Email address has potentially hostile characters in it."""
+    pass
+
+
+# Exceptions for admin request database
+class LostHeldMessage(MailmanError):
+    """Held message was lost."""
+    pass
