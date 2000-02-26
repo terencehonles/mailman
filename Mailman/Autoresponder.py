e240446392621e33b0b6ff6f3746eb6ebf88b7e9
bwarsaw <>
2000-02-26 00:09:35 +0000

    InitVars(): added autorespond_requests, autoresponse_request_text, and request_responses.
    GetConfigInfo(): I figured it /was/ worth adding an autoresponder on
    the -request address after all.  This works the same as the -admin,
    and postings autoresponder with one variation: you now have three
    choices
    
    1. no autoreply
    2. autoreply, but discard the original message
    3. autoreply, and forward the message off to the mail command
       processing robot
    
    This is useful for lists that essentially to shut off the command
    handler, but it may be overkill.  It's definitely useful to autoreply
    to the -request address after all.
    
    Also, I slightly reformated the HTML for this page, putting the common
    stuff up in the preamble.

--- Mailman/Autoresponder.py
+++ Mailman/Autoresponder.py
@@ -26,41 +26,58 @@ class Autoresponder:
         # configurable
         self.autorespond_postings = 0
         self.autorespond_admin = 0
+        # this value can be
+        #  0 - no autoresponse on the -request line
+        #  1 - autorespond, but discard the original message
+        #  2 - autorespond, and forward the message on to be processed
+        self.autorespond_requests = 0
         self.autoresponse_postings_text = ''
         self.autoresponse_admin_text = ''
+        self.autoresponse_request_text = ''
         self.autoresponse_graceperiod = 90 # days
         # non-configurable
         self.postings_responses = {}
         self.admin_responses = {}
+        self.request_responses = {}
 
     def GetConfigInfo(self):
         return [
-            'Auto-responder characteristics.',
+            """Auto-responder characteristics.<p>
+
+In the text fields below, Python %(string)s interpolation is performed with
+the following key/value substitutions:
+<p><ul>
+    <li><b>%(listname)s</b> - <em>gets the name of the mailing list</em>
+    <li><b>%(listurl)s</b> - <em>gets the list's listinfo URL</em>
+    <li><b>%(requestemail)s</b> - <em>gets the list's -request address</em>
+    <li><b>%(adminemail)s</b> - <em>gets the list's -admin address</em>
+</ul>
+
+<p>For each text field, you can either enter the text directly into the text
+box, or you can specify a file on your local system to upload as the text.""",
 
             ('autorespond_postings', mm_cfg.Toggle, ('No', 'Yes'), 0,
              'Should Mailman send an auto-response to mailing list posters?'),
 
             ('autoresponse_postings_text', mm_cfg.FileUpload, ('6', '30'), 0,
-             """Auto-response text to send to mailing list posters.
-Python %(string)s interpolation is performed on the text with the following
-key/value pairs.<p>
-<b>listname</b> <em>gets the name of the mailing list</em><br>
-<b>listurl</b> <em>gets the list's listinfo URL</em><br>
-<b>requestemail</b> <em>gets the list's -request address</em><br>
-<b>adminemail</b> <em>gets the list's -admin address</em>"""),
+             'Auto-response text to send to mailing list posters.'),
 
             ('autorespond_admin', mm_cfg.Toggle, ('No', 'Yes'), 0,
-             'Should Mailman send an auto-response to emails sent to the '
-             '-admin address?'),
+             '''Should Mailman send an auto-response to emails sent to the
+-admin address?'''),
 
             ('autoresponse_admin_text', mm_cfg.FileUpload, ('6', '30'), 0,
-             """Auto-response text to send to -admin emails.
-Python %(string)s interpolation is performed on the text with the following
-key/value pairs.<p>
-<b>listname</b> <em>gets the name of the mailing list</em><br>
-<b>listurl</b> <em>gets the list's listinfo URL</em><br>
-<b>requestemail</b> <em>gets the list's -request address</em><br>
-<b>adminemail</b> <em>gets the list's -admin address</em>"""),
+             'Auto-response text to send to -admin emails.'),
+
+            ('autorespond_requests', mm_cfg.Radio,
+             ('No', 'Yes, w/discard', 'Yes, w/forward'), 0,
+             '''Should Mailman send an auto-response to emails sent to the
+-request address?  If you choose yes, decide whether you want Mailman to
+discard the original email, or forward it on to the system as a normal mail
+command.'''),
+
+            ('autoresponse_request_text', mm_cfg.FileUpload, ('6', '30'), 0,
+             'Auto-response text to send to -request emails.'),
 
             ('autoresponse_graceperiod', mm_cfg.Number, 3, 0,
              '''Number of days between auto-responses to either the mailing
