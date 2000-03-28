5085949405294422bc55c7f35da69ebd1c09780c
bwarsaw <>
2000-03-28 17:32:09 +0000

    Implementation of file queing on total or partial delivery failures. Specifically,
    process(): move smptlib.SMTP() into the try/except so that
    socket.errors on the connection can be flagged and dealt with as total
    delivery failures.
    
    Better error logging.
    
    If the connection goes through but smtplib throws an
    SMTPRecipientsRefused exception, then Only persistent transient
    failures are queued, i.e. those with error code < 500.  For failed
    recipients with codes >= 500, these are permanent failures and we
    do a bounce registration instead.
    
    queue_message(): handles all the actual queuing of files for future
    re-delivery attempts.  Two files per message are written to
    $prefix/qfiles: one is a .txt containing the full text of the message,
    the other is a .db file containing a marshal of this information:
    
        listname - Python string naming the list for delivery
        recips   - Python list of addr strings
        attepts  - number of total delivery attempts for this queued message
        last_recip_count - number of recips the last time this delivery
    		       was re-tried (not sure how useful that will
    		       be).
    
    The base name of the two files is the SHA hexdigest hash of the
    message text.

--- Mailman/Handlers/SMTPDirect.py
+++ Mailman/Handlers/SMTPDirect.py
@@ -23,9 +23,15 @@ isn't locked while delivery occurs synchronously.
 
 """
 
+import os
 import time
 import socket
+import sha
+import marshal
+
 from Mailman import mm_cfg
+from Mailman import Utils
+from Mailman.Handlers import HandlerAPI
 from Mailman.pythonlib import smtplib
 
 
@@ -38,9 +44,9 @@ def process(mlist, msg):
     # diagnose whether we need to rewrite this module to relinquish the list
     # lock or not.
     t0 = time.time()
-    conn = smtplib.SMTP(mm_cfg.SMTPHOST, mm_cfg.SMTPPORT)
     refused = {}
     try:
+        conn = smtplib.SMTP(mm_cfg.SMTPHOST, mm_cfg.SMTPPORT)
         try:
             # make sure the connect happens, which won't be done by the
             # constructor if SMTPHOST is false
@@ -57,19 +63,65 @@ def process(mlist, msg):
     # MTA not responding, or other socket problems, or any other kind of
     # SMTPException.  In that case, nothing got delivered
     except (socket.error, smtplib.SMTPException), e:
-        mlist.LogMsg('smtp', 'Some other exception occurred:\n' + `e`)
-        mlist.LogMsg('smtp', 'All recipients refused')
+        mlist.LogMsg('smtp', 'All recipients refused: %s' % e)
+        # no recipients ever received the message
+        queue_message(mlist, msg)
     #
     # Go through all refused recipients and deal with them if possible
+    tempfailures = []
     for recip, (code, smtpmsg) in refused.items():
         if code >= 500:
             # It's a permanent failure for this recipient so register it.  We
             # don't save the list between each registration because we assume
             # it happens around the whole message delivery sequence
             mlist.RegisterBounce(recip, msg, saveifdirty=0)
-        # TBD: should we do anything with persistent transient failures, like
-        # queue them up and try to resend them?  Should we log the smtp error
-        # messages, or do any other processing of failures?  Lots of
-        # questions...
         else:
+            # deal with persistent transient failures by queuing them up for
+            # future delivery.
             mlist.LogMsg('smtp-failure', '%d %s (%s)' % (code, recip, smtpmsg))
+            tempfailures.append(recip)
+    if tempfailures:
+        queue_message(mlist, msg, tempfailures)
+
+
+
+def queue_message(mlist, msg, recips=None):
+    if recips is None:
+        # i.e. total delivery failure
+        recips = msg.recips
+    # calculate a unique name for this file
+    text = str(msg)
+    filebase = sha.new(text).hexdigest()
+    msgfile = os.path.join(mm_cfg.QUEUE_DIR, filebase + '.msg')
+    dbfile = os.path.join(mm_cfg.QUEUE_DIR, filebase + '.db')
+    # Initialize the information about this message delivery.  It's possible a
+    # delivery attempt has been previously tried on this message, in which
+    # case, we'll just update the data.  We should probably do /some/ timing
+    # out of failed deliveries.
+    try:
+        dbfp = open(dbfile)
+        msgdata = marshal.load(dbfp)
+        dbfp.close()
+        msgdata['last_recip_count'] = len(msgdata['recips'])
+        msgdata['recips'] = recips
+        msgdata['attempts'] = msgdata['attempts'] + 1
+        existsp = 1
+    except (EOFError, ValueError, TypeError, IOError):
+        msgdata = {'listname'        : mlist.internal_name(),
+                   'recips'          : recips,
+                   'attempts'        : 1,
+                   'last_recip_count': -1,
+                   # any other stats we need?
+                   }
+        existsp = 0
+    # write the data file
+    dbfp = Utils.open_ex(dbfile, 'w')
+    marshal.dump(msgdata, dbfp)
+    dbfp.close()
+    # if it doesn't already exist, write the message file
+    if not existsp:
+        msgfp = Utils.open_ex(msgfile, 'w')
+        msgfp.write(text)
+        msgfp.close()
+    # this is a signal to qrunner
+    msg.failedcount = len(recips)
