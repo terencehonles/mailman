bab84d36a4fda72b36ba267428db56fa5de3b5b4
bwarsaw <>
2000-07-22 02:12:29 +0000

    GetConfigInfo(): Document that the -admin and -owner addresses are equivalent for autoresponse purposes.

--- Mailman/Autoresponder.py
+++ Mailman/Autoresponder.py
@@ -53,6 +53,7 @@ the following key/value substitutions:
     <li><b>%(listurl)s</b> - <em>gets the list's listinfo URL</em>
     <li><b>%(requestemail)s</b> - <em>gets the list's -request address</em>
     <li><b>%(adminemail)s</b> - <em>gets the list's -admin address</em>
+    <li><b>%(owneremail)s</b> - <em>gets the list's -owner address</em>
 </ul>
 
 <p>For each text field, you can either enter the text directly into the text
@@ -67,11 +68,11 @@ box, or you can specify a file on your local system to upload as the text.""",
 
             ('autorespond_admin', mm_cfg.Toggle, ('No', 'Yes'), 0,
              '''Should Mailman send an auto-response to emails sent to the
--admin address?'''),
+-admin and -owner addresses?'''),
 
             ('autoresponse_admin_text', mm_cfg.FileUpload,
              (6, WIDTH), 0,
-             'Auto-response text to send to -admin emails.'),
+             'Auto-response text to send to -admin and -owner emails.'),
 
             ('autorespond_requests', mm_cfg.Radio,
              ('No', 'Yes, w/discard', 'Yes, w/forward'), 0,
@@ -86,7 +87,7 @@ command.'''),
 
             ('autoresponse_graceperiod', mm_cfg.Number, 3, 0,
              '''Number of days between auto-responses to either the mailing
-list or -admin address from the same poster.  Set to zero (or negative) for
-no grace period (i.e. auto-respond to every message).'''),
+list or -admin/-owner address from the same poster.  Set to zero (or negative)
+for no grace period (i.e. auto-respond to every message).'''),
 
             ]
