b873d3a88785e2cb77f99e2117014af81ddbd2d9
bwarsaw <>
2001-04-02 06:03:06 +0000

    Added PENDING_FILE_SCHEMA_VERSION.

--- Mailman/Version.py
+++ Mailman/Version.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # Mailman version
-VERSION = "2.1a1"
+VERSION = "2.1a2"
 
 # And as a hex number in the manner of PY_VERSION_HEX
 ALPHA = 0xa
@@ -30,7 +30,7 @@ MINOR_REV = 1
 MICRO_REV = 0
 REL_LEVEL = ALPHA
 # at most 15 beta releases!
-REL_SERIAL = 1
+REL_SERIAL = 2
 
 HEX_VERSION = ((MAJOR_REV << 24) | (MINOR_REV << 16) | (MICRO_REV << 8) |
                (REL_LEVEL << 4)  | (REL_SERIAL << 0))
@@ -40,3 +40,6 @@ DATA_FILE_VERSION = 23
 
 # qfile/*.db schema version number
 QFILE_SCHEMA_VERSION = 3
+
+# version number for the data/pending.db file schema
+PENDING_FILE_SCHEMA_VERSION = 1
