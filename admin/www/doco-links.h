8d438035a5d08c18c7a163580f2ed731d9015072
bwarsaw <>
2001-05-17 02:40:57 +0000

    Added the new "Internationalization" sidebar link

--- admin/www/doco-links.h
+++ admin/www/doco-links.h
@@ -4,4 +4,5 @@
 <li><a href="mgrs.html">List Managers</a>
 <li><a href="admins.html">Site&nbsp;Administrators</a>
 <li><a href="devs.html">Developers</a>
+<li><a href="i18n.html">Internationalization</a>
 <li><a href="otherdocs.html">Other Documentation</a>
