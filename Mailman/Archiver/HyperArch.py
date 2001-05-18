bcac4f1735b29a14c8001d6b2c9f5b16a7b5c6cb
bwarsaw <>
2001-05-18 19:19:30 +0000

    Remove an unnecessary import.

--- Mailman/Archiver/HyperArch.py
+++ Mailman/Archiver/HyperArch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 1998,1999,2000 by the Free Software Foundation, Inc.
+# Copyright (C) 1998,1999,2000,2001 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -45,8 +45,6 @@ import pipermail
 
 from Mailman import mm_cfg, EncWord
 from Mailman.Logging.Syslog import syslog
-
-from Mailman.Utils import mkdir
 
 gzip = None
 if mm_cfg.GZIP_ARCHIVE_TXT_FILES:
