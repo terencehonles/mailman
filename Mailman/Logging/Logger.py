a19e2254de33dc433ac9b78b46aeb975b0ef849f
bwarsaw <>
2001-06-02 05:14:47 +0000

    close(): When the file object is closed, set __fp to None so it gets re-opened at the next attempted write.

--- Mailman/Logging/Logger.py
+++ Mailman/Logging/Logger.py
@@ -1,4 +1,4 @@
-# Copyright (C) 1998,1999,2000 by the Free Software Foundation, Inc.
+# Copyright (C) 1998,1999,2000,2001 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -83,3 +83,4 @@ class Logger:
 	if not self.__fp:
 	    return
 	self.__get_f().close()
+        self.__fp = None
