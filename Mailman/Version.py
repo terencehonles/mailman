2bd68751c1e6c8820923d254a6f50901f87a057a
bwarsaw <>
2001-07-18 22:06:01 +0000

    Bump the version number to 2.1a3 and kick the DATA_FILE_VERSION to 30 so the new variables from versions.py will get installed.

--- Mailman/Version.py
+++ Mailman/Version.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # Mailman version
-VERSION = "2.1a2"
+VERSION = "2.1a3"
 
 # And as a hex number in the manner of PY_VERSION_HEX
 ALPHA = 0xa
@@ -30,13 +30,13 @@ MINOR_REV = 1
 MICRO_REV = 0
 REL_LEVEL = ALPHA
 # at most 15 beta releases!
-REL_SERIAL = 2
+REL_SERIAL = 3
 
 HEX_VERSION = ((MAJOR_REV << 24) | (MINOR_REV << 16) | (MICRO_REV << 8) |
                (REL_LEVEL << 4)  | (REL_SERIAL << 0))
 
 # config.db schema version number
-DATA_FILE_VERSION = 26
+DATA_FILE_VERSION = 30
 
 # qfile/*.db schema version number
 QFILE_SCHEMA_VERSION = 3
