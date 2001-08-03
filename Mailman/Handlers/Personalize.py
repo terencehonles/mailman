a95717fd40d8f04e651fef04a91f210eab0b713d
bwarsaw <>
2001-08-03 21:55:06 +0000

    intermediate

---
+++ Mailman/Handlers/Personalize.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2001 by the Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software 
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""Personalize the email message for delivery to each individual recipient.
+"""
+
+from Mailman import mm_cfg
+from Mailman.Queue.sbcache import get_switchboard
+
+
+
+def process(mlist, msg, msgdata):
+    if not mlist.personalize or msgdata.get('personalized'):
+        return
+    # Make a copy for each recipient, and re-queue the copies to the incoming
+    # queue.  Set the pipeline for the copies to just Decorate and
+    # ToOutgoing.  The normal queue on the original copy should take care of
+    # everything else.
+    newpipeline = ['Decorate', 'ToOutgoing']
+    inq = get_switchboard(mm_cfg.INQUEUE_DIR)
+
+    for member in msgdata.get('recips', []):
+        metadatacopy = msgdata.copy()
+        metadatacopy['pipeline'] = newpipeline
+        metadatacopy['recips'] = [member]
+        metadatacopy['personalized'] = 1
+        inq.enqueue(msg, metadatacopy, listname=mlist.internal_name())
+
+    # Don't let the normal ToOutgoing processing actually send the original
+    # copy.
+    del msgdata['recips']
