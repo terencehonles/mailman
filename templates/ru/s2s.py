a4bdb824b41beb7cc40d8ca3c80cdac6aa4e39ee
mss <>
2002-01-25 21:07:18 +0000

    preliminary russian translation for templates

---
+++ templates/ru/s2s.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+import sys, string
+from pprint import pprint
+
+def chop (line):
+    if line[-2:] == '\r\n':
+        line = line[:-2]
+
+    if line[-1:] == '\n':
+        line = line[:-1]
+
+    return line
+
+name = None
+revision = None
+
+files = {}
+
+for line in sys.stdin.readlines ():
+    parts = string.split (chop (line))
+
+    if len (parts) > 0:
+        if parts[0] == 'File:':
+            name = parts[1]
+        elif parts[0] == 'Repository':
+            files[name] = parts[2]
+
+# pprint (files)
+
+for line in open ('status', 'r').readlines ():
+    parts = string.split (chop (line))
+
+    if len (parts) > 0:
+        if files.has_key (parts[0]):
+            pass    # check the version
+
+            if files[parts[0]] != parts[1]:
+                print 'Update: %s (%s -> %s)' % (parts[0], parts[1], files[parts[0]])
+
+            del files[parts[0]] # delete the item
+        else:
+            print 'Delete:', parts[0]
+
+for file in files.keys ():
+    print 'New:', file
