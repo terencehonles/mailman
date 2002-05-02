ea909c059b1c1671adbb995d292649482277ad95
bwarsaw <>
2002-05-02 02:54:46 +0000

    New architecture for email commands.  Instead of the monolithic (and unmaintainable) MailCommandHandler.py file, we've now got a framework where each command is implemented in a separate file.  This means it's both more extensible and more flexible:
    - you can easily add new commands for things I haven't thought of
      <wink>, and the `help' command will automatically adjust
    
    - you can disable commands entirely by removing the appropriate file
    
    - you can disable, change, or add commands on a per-list (or even
      per-message or per-sender) basis
    
    CommandRunner.py is the module that calls into this framework.  Each
    command is implemented as a cmd_<command>.py file.  The `set' command
    is the most complicated.  The help text is currently implemented as
    module docstrings (for most commands), so the i18n catalogs must be
    updated.  Also the help.txt files will be updated.

---
+++ Mailman/Commands/cmd_confirm.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2002 by the Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+    confirm <confirmation-string>
+        Confirm an action.  The confirmation-string is required and should be
+        supplied with in mailback confirmation notice.
+"""
+
+from Mailman import mm_cfg
+from Mailman import Errors
+from Mailman.i18n import _
+
+STOP = 1
+
+
+
+def gethelp(mlist):
+    return _(__doc__)
+
+
+
+def process(res, args):
+    mlist = res.mlist
+    if len(args) <> 1:
+        res.results.append(_('Usage:'))
+        res.results.append(gethelp(mlist))
+        return STOP
+    cookie = args[0]
+    try:
+        results = mlist.ProcessConfirmation(cookie, res.msg)
+    except Errors.MMBadConfirmation, e:
+        # Express in approximate days
+        days = int(mm_cfg.PENDING_REQUEST_LIFE / mm_cfg.days(1) + 0.5)
+        res.results.append(_("""\
+Invalid confirmation string.  Note that confirmation strings expire
+approximately %(days)s days after the initial subscription request.  If your
+confirmation has expired, please try to re-submit your original request or
+message."""))
+        return STOP
+    except Errors.MMNeedApproval:
+        res.results.append(_("""\
+Your request has been forwarded to the list moderator for approval."""))
+    except Errors.MMAlreadyAMember:
+        # Some other subscription request for this address has
+        # already succeeded.
+        res.results.append(_('You are already subscribed.'))
+        return STOP
+    except Errors.MMNoSuchUserError:
+        # They've already been unsubscribed
+        res.results.append(_("""\
+You are not a member.  Have you already unsubscribed?"""))
+        return STOP
+    else:
+        res.results.append(_('Confirmation succeeded'))
