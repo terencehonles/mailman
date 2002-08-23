a4151aae864be733fd80546f7c20d8c23224dcc9
bwarsaw <>
2002-08-23 20:41:52 +0000

    Patch set for SF bug #596565.  Use symbolic user/group names instead of numeric ids.  Initial idea and patch by Todd Vierling, fleshed out by Barry.
    Specific changes here:
    
    We need to include grp.h
    
    check_caller()'s second argument now is a const char* instead of a
    GID_T, since it takes a group name not an id.
    
    GID_MISMATCH -> GROUP_MISMATCH
    
    Added exit code 8 for GROUP_NAME_NOT_FOUND which can happen if the
    symbolic group name isn't installed on the system.

--- src/common.h
+++ src/common.h
@@ -6,14 +6,14 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software 
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
@@ -25,6 +25,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <grp.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -32,25 +33,25 @@
 #define GID_T GETGROUPS_T
 
 extern void fatal(const char*, int, char*, ...);
-extern void check_caller(const char*, GID_T);
+extern void check_caller(const char*, const char*);
 extern int run_script(const char*, int, char**, char**);
 
-/* bogus global variable used as a flag */
+/* Global variable used as a flag. */
 extern int running_as_cgi;
 
-/* extern to reference this global from one of the wrapper mains */
+/* Extern to reference this global from one of the wrapper mains */
 extern const char* logident;
 
-
-/* exit codes, so it's easier to distinguish what caused fatal errors when
+/* Exit codes, so it's easier to distinguish what caused fatal errors when
  * looking at syslogs.
  */
-#define GID_MISMATCH 2
+#define GROUP_MISMATCH 2
 #define SETREGID_FAILURE 3
 #define EXECVE_FAILURE 4
 #define MAIL_USAGE_ERROR 5
 #define MAIL_ILLEGAL_COMMAND 6
 #define ADDALIAS_USAGE_ERROR 7
+#define GROUP_NAME_NOT_FOUND 8
 
 
 /*
