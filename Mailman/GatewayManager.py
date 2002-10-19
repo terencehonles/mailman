6f29c75600db585dd53860703639490ff6a85a8a
bwarsaw <>
2002-10-19 17:25:41 +0000

    InitVars(): New config variable news_moderation, which is a 3-way flag controlling how the mail->news gateway should handle moderation.  This supplants MM2.0's SF patch #401270 in a much simpler way.  Possible values are:
    - 0 means we're not gating to a moderated newsgroup
    - 1 means "open list, moderated group", essentially we just add an
      Approved header to the gated message, but do nothing else special
    - 2 means "moderated" which adds an additional hold criteria to the
      message.  Once the message is approved, it gets an Approved header
      and is gated.
    
    Note: this was a top request by the FSF, and essential for
    python.org.  I had a brainstorm about how to make this fairly simple
    to add so we're thawing the release for this one.

--- Mailman/GatewayManager.py
+++ Mailman/GatewayManager.py
@@ -4,14 +4,14 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """Mixin class for configuring Usenet gateway.
@@ -33,3 +33,6 @@ class GatewayManager:
         self.gateway_to_news = 0
         self.gateway_to_mail = 0
         self.news_prefix_subject_too = 1
+        # In patch #401270, this was called newsgroup_is_moderated, but the
+        # semantics weren't quite the same.
+        self.news_moderation = 0
