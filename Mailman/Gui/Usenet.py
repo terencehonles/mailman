186e5d7a851b1d8db89ae2ac085da392b199be5f
bwarsaw <>
2002-10-19 17:32:29 +0000

    GetConfigInfo(): Added news_moderation variable, which is used to select the moderation policy of the gated-to newsgroup.  Also, re-arranged and cleaned up the interface by adding section headers.

--- Mailman/Gui/Usenet.py
+++ Mailman/Gui/Usenet.py
@@ -4,14 +4,14 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 from Mailman import mm_cfg
@@ -27,10 +27,14 @@ class Usenet(GUIBase):
     def GetConfigInfo(self, mlist, category, subcat=None):
         if category <> 'gateway':
             return None
+
         WIDTH = mm_cfg.TEXTFIELDWIDTH
+        VERTICAL = 1
 
         return [
             _('Mail-to-News and News-to-Mail gateway services.'),
+
+            _('News server settings'),
 
             ('nntp_host', mm_cfg.String, WIDTH, 0,
              _('''The Internet address of the machine your News server is
@@ -51,15 +55,38 @@ class Usenet(GUIBase):
              _('''Should new posts to the newsgroup be sent to the mailing
              list?''')),
 
-            ('_mass_catchup', mm_cfg.Toggle, (_('No'), _('Yes')), 0,
-             _('Should Mailman perform a <em>catchup</em> on the newsgroup?'),
-             _('''When you tell Mailman to perform a catchup on the newsgroup,
-             this means that you want to start gating messages to the mailing
-             list with the next new message found.  All earlier messages on
-             the newsgroup will be ignored.  This is as if you were reading
-             the newsgroup yourself, and you marked all current messages as
-             <em>read</em>.  By catching up, your mailing list members will
-             not see any of the earlier messages.''')),
+            _('Forwarding options'),
+
+            ('news_moderation', mm_cfg.Radio,
+             (_('None'), _('Open list, moderated group'), _('Moderated')),
+             VERTICAL,
+
+             _("""The moderation policy of the newsgroup."""),
+
+             _("""This setting determines the moderation policy of the
+             newsgroup and its interaction with the moderation policy of the
+             mailing list.  This only applies to the newsgroup that you are
+             gatewaying <em>to</em>, so if you are only gatewaying from
+             Usenet, or the newsgroup you are gatewaying to is not moderated,
+             set this option to <em>None</em>.
+
+             <p>If the newsgroup is moderated, you can set this mailing list
+             up to be the moderation address for the newsgroup.  By selecting
+             <em>Moderated</em>, an additional posting hold will be placed in
+             the approval process.  All messages posted to the mailing list
+             will have to be approved before being sent on to the newsgroup,
+             or to the mailing list membership.
+
+             <p><em>Note that if the message has an <tt>Approved</tt> header
+             with the list's administrative password in it, this hold test
+             will be bypassed, allowing privileged posters to send messages
+             directly to the list and the newsgroup.</em>
+
+             <p>Finally, if the newsgroup is moderated, but you want to have
+             an open posting policy anyway, you should select <em>Open list,
+             moderated group</em>.  The effect of this is to use the normal
+             Mailman moderation facilities, but to add an <tt>Approved</tt>
+             header to all messages that are gatewayed to Usenet.""")),
 
             ('news_prefix_subject_too', mm_cfg.Toggle, (_('No'), _('Yes')), 0,
              _('Prefix <tt>Subject:</tt> headers on postings gated to news?'),
@@ -70,6 +97,19 @@ class Usenet(GUIBase):
              disable the prefix on gated messages.  Of course, if you turn off
              normal <tt>Subject:</tt> prefixes, they won't be prefixed for
              gated messages either.""")),
+
+            _('Mass catch up'),
+
+            ('_mass_catchup', mm_cfg.Toggle, (_('No'), _('Yes')), 0,
+             _('Should Mailman perform a <em>catchup</em> on the newsgroup?'),
+             _('''When you tell Mailman to perform a catchup on the newsgroup,
+             this means that you want to start gating messages to the mailing
+             list with the next new message found.  All earlier messages on
+             the newsgroup will be ignored.  This is as if you were reading
+             the newsgroup yourself, and you marked all current messages as
+             <em>read</em>.  By catching up, your mailing list members will
+             not see any of the earlier messages.''')),
+
             ]
 
     def _setValue(self, mlist, property, val, doc):
