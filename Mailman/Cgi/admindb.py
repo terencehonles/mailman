9cf7d3cbcc71975cf6332963a492d1edb1ab4c68
bwarsaw <>
2002-11-06 04:48:13 +0000

    show_pending_subs(): Always uncanonstr() the fullname, even if it's just an empty string.  This avoids seeing the u'' when no real name was given.

--- Mailman/Cgi/admindb.py
+++ Mailman/Cgi/admindb.py
@@ -283,8 +283,7 @@ def show_pending_subs(mlist, form):
             mlist.HandleRequest(id, mm_cfg.DISCARD)
         id = ids[0]
         time, addr, fullname, passwd, digest, lang = mlist.GetRecord(id)
-        if fullname:
-            fullname = Utils.uncanonstr(fullname, mlist.preferred_language)
+        fullname = Utils.uncanonstr(fullname, mlist.preferred_language)
         radio = RadioButtonArray(id, (_('Defer'),
                                       _('Approve'),
                                       _('Reject'),
