b1ed9ac41839747d703df2be119461392ffe8f11
bwarsaw <>
2002-12-27 03:38:42 +0000

    bump version.ht not download.ht now

--- admin/bin/Release.py
+++ admin/bin/Release.py
@@ -1,19 +1,19 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 1998,1999,2000,2001 by the Free Software Foundation, Inc.
+# Copyright (C) 1998,1999,2000,2001,2002 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """Manage releases of Mailman.
@@ -73,7 +73,7 @@ def releasedir(tagname=None):
         tmpdir = tempfile.gettempdir()
         _releasedir = os.path.join(tmpdir, 'mailman-' + tagname)
     return _releasedir
-        
+
 
 
 # CVS related commands
@@ -92,7 +92,7 @@ def tag_release(tagname, retag):
     print 'Tagging release with', relname, '...'
     option = ''
     if retag:
-	option = '-F'
+        option = '-F'
     cvsdo('tag %s %s' % (option, relname))
 
 def checkout(tagname, tail):
@@ -130,8 +130,8 @@ DATEMARK    = '<!-DATE--->'
 def do_bump(newvers):
     print 'doing bump...',
     # hack some files
-    for file in ('index.ht', 'download.ht'):
-        print '%s...' % file, 
+    for file in ('index.ht', 'version.ht'):
+        print '%s...' % file,
         fp = open(os.path.join('admin', 'www', file), 'r+')
         text = fp.read()
         parts = text.split(VERSIONMARK)
@@ -169,14 +169,14 @@ def do_bump(newvers):
 
 def main():
     try:
-	opts, args = getopt.getopt(sys.argv[1:], 'btTph',
+        opts, args = getopt.getopt(sys.argv[1:], 'btTph',
                                    ['bump', 'tag', 'TAG', 'package', 'help'])
     except getopt.error, msg:
-	usage(1, msg)
+        usage(1, msg)
 
     # required minor rev number
     if len(args) <> 1:
-	usage(1, 'tagname argument is required')
+        usage(1, 'tagname argument is required')
 
     tagname = args[0]
 
@@ -198,15 +198,15 @@ def main():
     bump = 0
 
     for opt, arg in opts:
-	if opt in ('-h', '--help'):
-	    usage(0)
-	elif opt in ('-t', '--tag'):
-	    tag = 1
-	elif opt in ('-T', '--TAG'):
-	    tag = 1
-	    retag = 1
-	elif opt in ('-p', '--package'):
-	    package = 1
+        if opt in ('-h', '--help'):
+            usage(0)
+        elif opt in ('-t', '--tag'):
+            tag = 1
+        elif opt in ('-T', '--TAG'):
+            tag = 1
+            retag = 1
+        elif opt in ('-p', '--package'):
+            package = 1
         elif opt in ('-b', '--bump'):
             bump = 1
 
