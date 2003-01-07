d2761a2b7406a07022aa3abb2b5e6b1d39d41bd4
bwarsaw <>
2003-01-07 05:39:22 +0000

    bulkdeliver(): We've had reports of an IOError (code == 4, interrupted system call) during the conn.sendmail() call.  This just sets the message up to retry later.

--- Mailman/Handlers/SMTPDirect.py
+++ Mailman/Handlers/SMTPDirect.py
@@ -1,17 +1,17 @@
-# Copyright (C) 1998,1999,2000,2001,2002 by the Free Software Foundation, Inc.
+# Copyright (C) 1998-2003 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """Local SMTP direct drop-off.
@@ -337,7 +337,7 @@ def bulkdeliver(mlist, msg, msgdata, envsender, failures, conn):
         refused = e.recipients
     # MTA not responding, or other socket problems, or any other kind of
     # SMTPException.  In that case, nothing got delivered
-    except (socket.error, smtplib.SMTPException), e:
+    except (socket.error, smtplib.SMTPException, IOError), e:
         # BAW: should this be configurable?
         syslog('smtp', 'All recipients refused: %s', e)
         # If the exception had an associated error code, use it, otherwise,
