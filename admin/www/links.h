3aaa4bcc266bf9038257a295de3eaf3869772f81
bwarsaw <>
2003-03-14 18:01:37 +0000

    web page updates and subpages for rants

--- admin/www/links.h
+++ admin/www/links.h
@@ -2,7 +2,7 @@
 <h3>Overview</h3>
 <li><a href="index.html">Home</a>
 <li><a href="features.html">Features</a>
-<li><a href="otherstuff.html">Logos and Papers</a>
+<li><a href="otherstuff.html">Rants, Papers, and Logos</a>
 <li><a href="inthenews.html">Mailman in Use</a>
 <li><a href="prev.html">Previous Releases</a>
 <li><a href="bugs.html">Bugs and Patches</a>
