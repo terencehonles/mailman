69450802dc50cfb7a40b5fbd26af40fe433119b2
bwarsaw <>
2003-03-23 05:33:40 +0000

    Updates

--- admin/www/links.h
+++ admin/www/links.h
@@ -2,6 +2,7 @@
 <h3>Overview</h3>
 <li><a href="index.html">Home</a>
 <li><a href="features.html">Features</a>
+<li><a href="i18n.html">Internationalization</a>
 <li><a href="otherstuff.html">Rants, Papers, and Logos</a>
 <li><a href="inthenews.html">Mailman in Use</a>
 <li><a href="prev.html">Previous Releases</a>
