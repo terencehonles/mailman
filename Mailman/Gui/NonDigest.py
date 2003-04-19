c7e43fc2600e0c1ac63157ac0addcada6703893d
bwarsaw <>
2003-04-19 22:14:16 +0000

    Whitespace normalization.
    The last checkin fixed SF bug # 667167

--- Mailman/Gui/NonDigest.py
+++ Mailman/Gui/NonDigest.py
@@ -1,17 +1,17 @@
-# Copyright (C) 2001,2002 by the Free Software Foundation, Inc.
+# Copyright (C) 2001-2003 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """GUI component for managing the non-digest delivery options.
@@ -128,7 +128,7 @@ and footers:
              _('Header added to mail sent to regular list members'),
              _('''Text prepended to the top of every immediately-delivery
              message. ''') + headfoot + extra),
-            
+
             ('msg_footer', mm_cfg.Text, (10, WIDTH), 0,
              _('Footer added to mail sent to regular list members'),
              _('''Text appended to the bottom of every immediately-delivery
