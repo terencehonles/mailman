bc5308bb714aa4367570bdf10893161c409e2d00
bwarsaw <>
2003-04-22 01:57:33 +0000

    process(): When the list is anonymous, delete X-Originating-Email also.  Apparently this is a header that Hotmail adds.  Closes patch #725369 by Roger Tsang.

--- Mailman/Handlers/Cleanse.py
+++ Mailman/Handlers/Cleanse.py
@@ -1,17 +1,17 @@
-# Copyright (C) 1998,1999,2000,2001,2002 by the Free Software Foundation, Inc.
+# Copyright (C) 1998-2003 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 """Cleanse certain headers from all messages."""
@@ -29,6 +29,8 @@ def process(mlist, msg, msgdata):
         del msg['from']
         del msg['reply-to']
         del msg['sender']
+        # Hotmail sets this one
+        del msg['x-originating-email']
         msg['From'] = mlist.GetListEmail()
         msg['Reply-To'] = mlist.GetListEmail()
     # Some headers can be used to fish for membership
