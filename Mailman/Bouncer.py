2edb7239a19a04a703191dc4b0f763eb6ba297a1
bwarsaw <>
2005-12-06 22:55:01 +0000

    Whitespace normalization.

--- Mailman/Bouncer.py
+++ Mailman/Bouncer.py
@@ -12,10 +12,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+# USA.
 
-"""Handle delivery bounces.
-"""
+"""Handle delivery bounces."""
 
 import sys
 import time
@@ -152,7 +152,7 @@ class Bouncer:
         # check to see if the disable-by-bounce threshold has been reached.
         if info.score >= self.bounce_score_threshold:
             if mm_cfg.VERP_PROBES:
-                syslog('bounce', 
+                syslog('bounce',
                    'sending %s list probe to: %s (score %s >= %s)',
                    self.internal_name(), member, info.score,
                    self.bounce_score_threshold)
