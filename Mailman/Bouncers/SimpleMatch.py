385e418aff6456c52d08a62483c6f6ad979ec17c
msapiro <>
2006-01-09 17:52:32 +0000

    Add 'full mailbox' from Prodigy.net

--- Mailman/Bouncers/SimpleMatch.py
+++ Mailman/Bouncers/SimpleMatch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 1998-2003 by the Free Software Foundation, Inc.
+# Copyright (C) 1998-2006 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -12,7 +12,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+# USA.
 
 """Recognizes simple heuristically delimited bounces."""
 
@@ -74,6 +75,10 @@ PATTERNS = [
     (_c('This message was created automatically by mail delivery'),
      _c('^---- START OF RETURNED MESSAGE ----'),
      _c("addressed to '(?P<addr>[^']*)'")),
+    # Prodigy.net full mailbox
+    (_c("User's mailbox is full:"),
+     _c('Unable to deliver mail.'),
+     _c("User's mailbox is full:\s*<(?P<addr>.*)>.*$")),
     # Next one goes here...
     ]
 
