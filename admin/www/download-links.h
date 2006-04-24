8220f6d1277842a7c77cf7197876b0a7c4f52914
bwarsaw <>
2006-04-24 13:33:08 +0000

    Move the admin directory outside the main source directory

--- admin/www/download-links.h
+++
@@ -1,6 +0,0 @@
-<!-- -*- html -*- -->
-<h3>Downloading</h3>
-<li><a href="requirements.html">Requirements</a>
-<li><a href="download.html">Downloading</a>
-<li><a href="install.html">Installing</a>
-<li><a href="version.html">Latest Version</a>
