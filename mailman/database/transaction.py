2aee920dd9b9f522c4358b600a007315b7f57667
Barry Warsaw <barry@python.org>
2008-03-13 09:00:20 -0400

    Move mailman.database.txnsupport to mailman.database.transaction and update the txn() decorator.  It no longer requires a _withtxn() method since it knows how to commit and abort the current transaction using the global database.
    Flesh out the lmtp.txt doctest with documentation for both bogus and valid
    subaddresses.

--- mailman/database/transaction.py
+++ mailman/database/transaction.py
@@ -15,20 +15,37 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
 # USA.
 
-# A transaction wrapping decorator. The basic idea is that methods in the
-# DBContext that need to operate on transaction boundaries can be written to
-# be transaction naive.  By wrapping them in this decorator, they
-# automatically become transaction safe.
+"""Transactional support."""
 
+__metaclass__ = type
+__all__ = [
+    'txn',
+    ]
+
+
+from mailman.configuration import config
+
+
+
 class txn(object):
-    def __init__(self, func):
-        # func is a function object, not a method (even an unbound method).
-        self._func = func
+    """Decorator for transactional support.
+
+    When the function this decorator wraps exits cleanly, the current
+    transaction is committed.  When it exits uncleanly (i.e. because of an
+    exception, the transaction is aborted.
+
+    Either way, the current transaction is completed.
+    """
+    def __init__(self, function):
+        self._function = function
 
     def __get__(self, obj, type=None):
-        # Return a wrapper function that creates a bound method from the
-        # function, then calls it wrapped in a transaction boundary.  Uses a
-        # non-public method called _withtxn() in the object's class.
         def wrapper(*args, **kws):
-            return obj._withtxn(self._func.__get__(obj), *args, **kws)
+            try:
+                rtn = self._function(obj, *args, **kws)
+                config.db.commit()
+                return rtn
+            except:
+                config.db.abort()
+                raise
         return wrapper
