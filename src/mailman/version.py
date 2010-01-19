a078172e33e3a06ac744737891e4d8509cce6222
Barry Warsaw <barry@list.org>
2010-01-19 07:48:28 -0500

    Prep for 3.0a6.

--- src/mailman/version.py
+++ src/mailman/version.py
@@ -18,8 +18,8 @@
 """Mailman version strings."""
 
 # Mailman version
-VERSION = "3.0.0a5"
-CODENAME = 'Distant Early Warning'
+VERSION = "3.0.0a6"
+CODENAME = 'Cut to the Chase'
 
 # And as a hex number in the manner of PY_VERSION_HEX
 ALPHA = 0xa
@@ -34,7 +34,7 @@ MINOR_REV = 0
 MICRO_REV = 0
 REL_LEVEL = ALPHA
 # at most 15 beta releases!
-REL_SERIAL = 5
+REL_SERIAL = 6
 
 HEX_VERSION = ((MAJOR_REV << 24) | (MINOR_REV << 16) | (MICRO_REV << 8) |
                (REL_LEVEL << 4)  | (REL_SERIAL << 0))
