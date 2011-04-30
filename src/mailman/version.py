a90ca4ce77d99460ffd36a366cea0162869df783
Barry Warsaw <barry@list.org>
2011-04-30 18:19:43 -0400

    * Fix styleguide (given by Mark Sapiro). * Post release version and NEWS file tweaks.

--- src/mailman/version.py
+++ src/mailman/version.py
@@ -18,8 +18,8 @@
 """Mailman version strings."""
 
 # Mailman version
-VERSION = "3.0.0a7"
-CODENAME = 'Mission'
+VERSION = '3.0.0a7+'
+CODENAME = "Where's My Thing?"
 
 # And as a hex number in the manner of PY_VERSION_HEX
 ALPHA = 0xa
@@ -34,7 +34,7 @@ MINOR_REV = 0
 MICRO_REV = 0
 REL_LEVEL = ALPHA
 # at most 15 beta releases!
-REL_SERIAL = 7
+REL_SERIAL = 8
 
 HEX_VERSION = ((MAJOR_REV << 24) | (MINOR_REV << 16) | (MICRO_REV << 8) |
                (REL_LEVEL << 4)  | (REL_SERIAL << 0))
