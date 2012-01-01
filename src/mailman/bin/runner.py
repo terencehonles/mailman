0ff2f2e1aee0945b50e9c76f462cd376b6985818
Barry Warsaw <barry@list.org>
2012-01-01 14:14:46 -0500

    copybump

--- src/mailman/bin/runner.py
+++ src/mailman/bin/runner.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2011 by the Free Software Foundation, Inc.
+# Copyright (C) 2001-2012 by the Free Software Foundation, Inc.
 #
 # This file is part of GNU Mailman.
 #
--- src/mailman/runners/retry.py
+++ src/mailman/runners/retry.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2003-2011 by the Free Software Foundation, Inc.
+# Copyright (C) 2003-2012 by the Free Software Foundation, Inc.
 #
 # This file is part of GNU Mailman.
 #
