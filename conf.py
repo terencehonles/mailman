4972b50ec372f7c2e957609cdacd84fdacc32596
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-03-14 04:38:18 +0000

    make readthedocs happy, and remove the sphinx artifact directories not needed anymore

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ import sys, os
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -122,7 +122,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -222,6 +222,7 @@ def index_html():
     import errno
     cwd = os.getcwd()
     try:
+        os.makedirs('build/sphinx/html')
         os.chdir('build/sphinx/html')
         os.symlink('README.html', 'index.html')
         print 'index.html -> README.html'
